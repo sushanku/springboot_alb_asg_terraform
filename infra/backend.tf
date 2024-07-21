b9c93f4b8d5ee96af70440f7dadea81f0ca2edde
Sushan Kunwar <sushankunwar01@gmail.com>
2024-07-21 15:10:56 +0545

    s3 bucket name change

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-springboot-app-bucket"
+    bucket = "terraform-springboot-app-bucket2"
     key    = "terraform-state"
     region = "us-east-1"
     encrypt = true
