dd63c1db2e2ba35ec65e311fd7c4ad4e97f9950e
Sushan Kunwar <sushankunwar01@gmail.com>
2024-07-21 16:20:28 +0545

    ASG instance_refresh will rolling strategy implement

--- infra/asg.tf
+++ infra/asg.tf
@@ -12,6 +12,13 @@ resource "aws_autoscaling_group" "springboot-app" {
     version = "$Latest"
   }
 
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+  }
+
   tag {
     key                 = "Name"
     value               = "springboot-app-asg-instance"
